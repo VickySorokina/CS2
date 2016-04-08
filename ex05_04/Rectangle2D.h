46e57f85a225b7d97ce9b2f157e9db74bdd355af
VickySorokina <tikivicky@gmail.com>
2016-04-07 22:53:24 -0700

    some edits to rectangle h

--- ex05_04/Rectangle2D.h
+++ ex05_04/Rectangle2D.h
@@ -32,24 +32,24 @@ class Rectangle2D
 {
 public:
 	Rectangle2D();
-	Rectangle2D(double newX, double newY, double newH, double newW);
+	Rectangle2D(double, double, double, double);
 
 	double getX() const;
 	double getY() const;
-	void setX(double newX);
-	void setY(double newY);
+	void setX(double);
+	void setY(double);
 
 	double getWidth() const;
 	double getHeight() const;
-	void setWidth(double newWidth);
-	void setHeight(double newHeight);
+	void setWidth(double);
+	void setHeight(double);
 
 	double getArea() const;
 	double getPerimeter() const;
 
-	bool contains(double x, double y) const;
-	bool contains(const Rectangle2D &r) const;
-	bool overlaps(const Rectangle2D &r) const;
+	bool contains(double, double) const;
+	bool contains(const Rectangle2D &) const;
+	bool overlaps(const Rectangle2D &) const;
 
 private: 
 	double x;
